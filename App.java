8702aa3cdace33234ccc9dbd47909bdaaf69bda1
valentinauceanu <valentinauceanu15@gmail.com>
2022-05-13 09:26:57 +0300

    Testat unele metode din Trainer

--- App.java
+++ App.java
@@ -5,5 +5,11 @@ public class App {
         t.modifyTrainingDetails("maine", "la mama");
         System.out.println();
         t.trainingDetails();
+        Trainer tr = new Trainer();
+        System.out.println(tr.checkAvalability("15.02"));
+        tr.addMaterials(t.chapters,"Iluminatii");
+        for(int i = 0; i < t.chapters.size(); i++){
+            System.out.println(t.chapters.get(i));
+        }
     }
 }
